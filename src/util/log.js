89b632db4ca07bc3d0d9dbf6beb890c68034ee1f
RBLU <reto.blunschi@youpers.com>
2014-09-25 16:27:14 +0200

    initial files

--- src/util/log.js
+++ src/util/log.js
@@ -1,43 +1,45 @@
 var bunyan = require('bunyan');
 var bsyslog = require('bunyan-syslog');
 
-var config = require('../config/config');
+var getLogger = function (config) {
 
-var logConf = config.log;
+    var logConf = config.log;
 
-var loggerOptions = {
-    name: "Main",
-    streams: logConf.streams || [],
-    serializers: bunyan.stdSerializers
-};
-
-if (logConf.stdout) {
-    loggerOptions.streams.push({
-        stream: process.stdout,
-        level: logConf.stdout
-    });
-}
-
-if (logConf.syslog) {
-    var mySyslogStream = {
-        level: 'debug',
-        type: 'raw',
-        stream: bsyslog.createBunyanStream({
-            type: 'sys',
-            facility: logConf.syslog.facility || bsyslog.local0,
-            host: logConf.syslog.host,
-            port: logConf.syslog.port
-        })
+    var loggerOptions = {
+        name: "Main",
+        streams: logConf.streams || [],
+        serializers: bunyan.stdSerializers
     };
-    loggerOptions.streams.push(mySyslogStream);
-}
 
-if (logConf.stream) {
-    loggerOptions.streams.push(logConf.stream);
-}
+    if (logConf.stdout) {
+        loggerOptions.streams.push({
+            stream: process.stdout,
+            level: logConf.stdout
+        });
+    }
 
-var logger = bunyan.createLogger(loggerOptions);
+    if (logConf.syslog) {
+        var mySyslogStream = {
+            level: 'debug',
+            type: 'raw',
+            stream: bsyslog.createBunyanStream({
+                type: 'sys',
+                facility: logConf.syslog.facility || bsyslog.local0,
+                host: logConf.syslog.host,
+                port: logConf.syslog.port
+            })
+        };
+        loggerOptions.streams.push(mySyslogStream);
+    }
+
+    if (logConf.stream) {
+        loggerOptions.streams.push(logConf.stream);
+    }
+
+    var logger = bunyan.createLogger(loggerOptions);
+    return logger;
+}
 
 module.exports = {
-    logger: logger
+    getLogger: getLogger
 };
