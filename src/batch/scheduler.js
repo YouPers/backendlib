5ec083e9f12ac3c4ddedab32bd08a044e7d96690
Boris <boris.wechselberger@youpers.com>
2015-05-17 08:27:48 +0200

    WL-2024

--- src/batch/scheduler.js
+++ src/batch/scheduler.js
@@ -4,6 +4,12 @@ var cronJob = require('cron').CronJob,
 var scheduleJobs = function scheduleJobs(jobs, log, config) {
 
     _.forEach(jobs, function scheduleJob(job) {
+        var env = process.env.NODE_ENV || 'development';
+        // check if the job is restricted to a subset of environments it should be executed on
+        if(job.environments && !_.contains(job.environments, env)) {
+            log.info('skipping Job: "' + job.name + '" - env "' + env + '" is not in the list of specified environments:' + job.environments);
+            return;
+        }
         job.context.name = job.name;
         job.context.description = job.description;
         job.context.cronTime = job.cronTime;
