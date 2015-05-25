87a09f6cea89282a468195a0cd76a8194102b84e
RBLU <reto.blunschi@youpers.com>
2015-05-25 08:59:25 +0200

    added error handling for stream errors in logger

--- src/util/log.js
+++ src/util/log.js
@@ -21,16 +21,24 @@ var getLogger = function (config) {
     }
 
     if (logConf.logstash) {
+
+        var stream = bLogstashTcp.createStream({
+            host: logConf.logstash.host || 'localhost',
+            port: logConf.logstash.port || 5001,
+            max_connect_retries: -1,
+            retry_interval: 2000,
+            cbuffer_size: 100
+        });
+
+        stream.on('error', function(err) {
+            var date = new Date();
+            console.log(date.toISOString() + ' - caught logstash stream error event, reconnection troubles, ENV: ' + process.env.NODE_ENV + ' err: ' + JSON.stringify(err));
+        });
+
         var myLogstashStream = {
                 level: logConf.logstash.level || 'debug',
                 type: "raw",
-                stream: bLogstashTcp.createStream({
-                    host: logConf.logstash.host || 'localhost',
-                    port: logConf.logstash.port || 5001,
-                    max_connect_retries: -1,
-                    retry_interval: 2000,
-                    cbuffer_size: 100
-                })
+                stream: stream
             };
         loggerOptions.streams.push(myLogstashStream);
     }
